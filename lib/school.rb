448583bc219b49ef193a4e18ae53927298cfa356
Chris Nunez <cenunez47@gmail.com>
2017-09-26 16:44:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name, roster={})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student
+  end
+
+  def grade
+  end
+
+  def sort
+  end
+
+end
